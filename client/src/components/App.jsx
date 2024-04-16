48aa1108d979812502795a70ea32eb99e4ca63ee
Tech-goodat <felixkiprotich2000@gmail.com>
2024-04-16 16:44:11 +0300

    done with client-side routing

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './Home'
+import Assets from './Assets'
+import Client from './Client'
+import Holdings from './Holdings'
+import Login from './Login'
+import Transactions from './Transactions'
+import Accounts from './Accounts'
+import Navbar from './NavBar'
+
 
 function App() {
   return (
-    <div>App</div>
+    <Router>
+      <div>
+        <Navbar />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/assets" element={<Assets />} />
+        <Route path="/client" element={<Client />} />
+        <Route path="/holdings" element={<Holdings />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/transactions" element={<Transactions />} />
+        <Route path="/accounts" element={<Accounts />} />
+      </Routes>
+    </Router>
   )
 }
 
