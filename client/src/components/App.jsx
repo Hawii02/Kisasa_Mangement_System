3bfa6aaf8fbb856e54ecfb4a0ad4f0a6b2c6959e
Tech-goodat <felixkiprotich2000@gmail.com>
2024-04-16 18:05:07 +0300

    created a sidebar component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -13,9 +13,13 @@ import Navbar from './NavBar'
 function App() {
   return (
     <Router>
-      <div>
-        <Navbar />
-      </div>
+       <div className="bg-[#f9fafb]  rounded-lg ">
+       <div className="md:flex h-screen overflow-hidden">
+       <div className="hidden md:hidden lg:flex">
+       <Navbar />
+        </div>
+       
+          <div className="flex-1 overflow-auto lg:ml-[30px]  mt-[90px] lg:pr-3 text-[#022c22] ">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/assets" element={<Assets />} />
@@ -23,8 +27,12 @@ function App() {
         <Route path="/holdings" element={<Holdings />} />
         <Route path="/login" element={<Login />} />
         <Route path="/transactions" element={<Transactions />} />
-        <Route path="/accounts" element={<Accounts />} />
+        <Route path="/accounts" element={<Accounts />} /> 
       </Routes>
+      </div>
+      </div>
+      </div>
+     
     </Router>
   )
 }
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { BiHomeSmile } from "react-icons/bi";
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div className='bg-[#022c22]  text-white w-[200px] h-screen flex items-center justify-center'>NavBar</div>
   )
 }
 
