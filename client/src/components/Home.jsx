3c413febf930a60e4b04ffc5172e891ec8ed1927
Tech-goodat <felixkiprotich2000@gmail.com>
2024-04-16 22:30:41 +0300

    designed the home page and navbar

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='grid grid-cols-2 gap-4 items-center w-full h-full'>
+      <div className='flex flex-col h-full  w-full d rounded-lg items-center '>
+        <div className='flex flex-col mb-9  items-center justify-center w-full text-5xl font-bold'>
+        <h1 className='text-center'> Banking made</h1>
+        <h1 className='text-center'> Easy, aytime</h1>
+        <h1 className='text-center text-[#f59e0b] hover:text-inherit'> Every Time</h1>
+        </div>
+        <div className=' w-full flex  items-center justify-center '>
+          <p className='text-center text-sm items-center w-full'>
+            "Empowering your financial journey<br /> with personalized solutions and expert guidance.<br/>
+             Securely managing your money while helping you achieve your goals. "</p>
+        </div>
+        <div className='flex w-full mt-11 gap-11 items-center justify-center'>
+          <button className='text-white bg-[#022c22] p-3 w-[130px] rounded-full font-bold hover:bg-[#f59e0b] hover:text-inherit'>Learn more</button>
+          <button className='text-inherit bg-[#f59e0b] p-3 w-[130px] rounded-full font-bold hover:bg-[#022c22] hover:text-white'>Dashboard</button>
+        </div>
+       
+      </div>
+
+      <div className='flex h-full custom_bg_home  rounded-lg items-center justify-center'>
+        <h1 className=' w-full h-full '></h1>
+      </div>
+    </div>
   )
 }
 
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,14 +6,17 @@ import { GrTransaction } from "react-icons/gr";
 import { FaHandsHolding } from "react-icons/fa6";
 import { MdOutlineManageAccounts } from "react-icons/md";
 import { BsPersonFillCheck } from "react-icons/bs";
+import { FaShopLock } from "react-icons/fa6";
 
 function NavBar() {
   return (
-    <div className='bg-[#022c22]   text-white w-[200px] h-screen flex flex-col '>
-      <div className='flex items-center justify-center w-full h-[160px] '>
+    <div className='bg-white  w-[200px] text-[#022c22] font-bold h-screen flex flex-col shadow-md '>
+      <div className='flex flex-col mb-9 items-center justify-center w-full h-[160px] '>
+      <FaShopLock  className='text-8xl'/> 
+      <h1 className='text-xl'>Kis<span className='text-2xl text-[#f59e0b] font-bold '>A</span>sa</h1>
 
       </div>
-      <div className=' flex ml-7  flex-col w-full'>
+      <div className=' flex ml-9  flex-col w-full '>
         <div className='w-full mb-7 flex items-center justify-start'><Link to='/' className='flex items-center justify-center'><BiHomeSmile className='mr-3' /> <span>Home</span></Link></div>
         <div className='w-full mb-7 flex items-center justify-start'> <Link to='/assets' className='flex items-center justify-center'><MdOutlineInventory  className='mr-3'/> <span>Assets</span></Link></div>
         <div className='w-full mb-7 flex items-center justify-start'><Link to='/transactions' className='flex items-center justify-center'><GrTransaction className='mr-3'/> <span>Transactions</span></Link></div>
