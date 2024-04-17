bb0a47bdbaabd34095a5c7c4edbfd637a9c9808c
leon <onditileon21@gmail.com>
2024-04-17 11:22:18 +0300

    Added logged in

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { useState } from "react";
 
-function Login() {
+function Login({ onLogin }) {
+  const [username, setUsername] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const response = await fetch("/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username }),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to login");
+      }
+      const user = await response.json();
+      onLogin(user);
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
   return (
-    <div>Login</div>
-  )
+    <form onSubmit={handleSubmit}>
+      <h3>Login With Username</h3>
+      <label htmlFor="username">Username: </label>
+      <input
+        type="text"
+        id="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <button type="submit">Login</button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+    </form>
+  );
 }
 
-export default Login
+export default Login;
