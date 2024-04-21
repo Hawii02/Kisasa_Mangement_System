332e65cd9c74a82fed164cbb7b30c6aefade7c03
norahkibathi <norah.njeri@student.moringaschool.com>
2024-04-22 01:24:22 +0300

    updated backend

--- server/models.py
+++ server/models.py
@@ -1,8 +1,36 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.hybrid import hybrid_property
+from flask_bcrypt import Bcrypt 
 from datetime import datetime
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+class Admin(db.Model):
+    __tablename__ = 'admin'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    _password_hash = db.Column(db.String, nullable=False)
+
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+    
+    @password_hash.setter
+    def password_hash(self,userpassword):
+        hashed_value = bcrypt.generate_password_hash(userpassword.encode('utf-8'))
+        self._password_hash = hashed_value.decode('utf-8')
+
+    def authenticate(self,userpassword):
+        is_valid = bcrypt.check_password_hash(self._password_hash, userpassword.encode('utf-8'))
+        return is_valid
+    
+class TokenBlocklist(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    jti=db.Column(db.String, nullable=False, index=True)
+    created_At = db.Column(db.DateTime, default=datetime.utcnow)
 
 class Client(db.Model):
     __tablename__ = 'clients'
@@ -10,11 +38,14 @@ class Client(db.Model):
     first_name = db.Column(db.String(50))  
     last_name = db.Column(db.String(50))
     email = db.Column(db.String(100))
+    account_id = db.Column(db.Integer, db.ForeignKey('accounts.id'))
 
     accounts = db.relationship('Account', back_populates='client')
     transactions = db.relationship("Transactions", back_populates="client")
     assets = db.relationship('Asset', back_populates='client')
     holdings = db.relationship('Holdings', back_populates='client')
+
+    accounts = db.relationship('Account', back_populates='client', foreign_keys='Account.client_id')  
 
     def to_dict(self):
         return {
@@ -22,48 +53,54 @@ class Client(db.Model):
             'first_name': self.first_name,
             'last_name': self.last_name,
             'email': self.email,
+            'account_id': self.account_id,
             'accounts': [account.to_dict() for account in self.accounts],
             'assets': [asset.to_dict() for asset in self.assets],
             'transactions': [transaction.to_dict() for transaction in self.transactions],
             'holdings': [holding.to_dict() for holding in self.holdings]
         }
 
-
     def __repr__(self):
-        return f'<Client {self.id}, {self.first_name}, {self.last_name}, {self.email}>'
+        return f'<Client {self.id}, {self.first_name}, {self.last_name}, {self.email}, {self.account_id}>'
 
 class Account(db.Model, SerializerMixin):
     __tablename__ = 'accounts'
 
     id = db.Column(db.Integer, primary_key=True)
     account_number = db.Column(db.String(20))
-   
-    client_id = db.Column(db.Integer, db.ForeignKey('clients.id'))
+    acc_balance = db.Column(db.Float)
+    client_id = db.Column(db.Integer, db.ForeignKey('clients.id', use_alter=True))
+    client_name = db.Column(db.String(100))
 
     client = db.relationship('Client', back_populates='accounts')
     holdings = db.relationship('Holdings', back_populates='account')
     transactions = db.relationship('Transactions', back_populates='account')
 
+    client = db.relationship('Client', back_populates='accounts', foreign_keys=[client_id])  
+
     def to_dict(self):
         return {
             'id': self.id,
             'account_number': self.account_number,
-            'client_id': self.client_id
+            'acc_balance': self.acc_balance,
+            'client_id': self.client_id,
+            'client_name': self.client_name
         }
 
     def __repr__(self):
-        return f'<Account {self.id}, {self.account_number}>'
+        return f'<Account {self.id}, {self.account_number}, {self.client_id}, {self.client_name}, {self.acc_balance}>'
 
 class Transactions(db.Model, SerializerMixin):
     __tablename__ = "transactions"
 
     id = db.Column(db.Integer, primary_key=True)
     transaction_type = db.Column(db.String)
-    transaction_amount = db.Column(db.Integer)
+    transaction_amount = db.Column(db.Float(precision=2))
     transaction_date = db.Column(db.DateTime, default=datetime.utcnow)
     
     account_id = db.Column(db.Integer, db.ForeignKey('accounts.id'))
-    client_id = db.Column(db.Integer, db.ForeignKey('clients.id')) 
+    client_id = db.Column(db.Integer, db.ForeignKey('clients.id'))
+    client_name = db.Column(db.String)
 
     client = db.relationship("Client", back_populates="transactions")
     account = db.relationship("Account", back_populates="transactions")
@@ -76,15 +113,12 @@ class Transactions(db.Model, SerializerMixin):
             'transaction_type': self.transaction_type,
             'transaction_amount': self.transaction_amount,
             'transaction_date': self.transaction_date.isoformat(),
-            'client': {
-                'id': self.client.id,
-                'name': self.client.name,
-                'email': self.client.email
-            }
+            'client': self.client.id,
         }
+                
 
     def __repr__(self):
-        return f'<Transactions {self.id}, {self.transaction_type}, {self.transaction_amount}, {self.transaction_date},{self.client_id}>'
+        return f'<Transactions {self.id}, {self.transaction_type}, {self.transaction_amount}, {self.transaction_date},{self.client_id}, {self.client_name}>'
 
 class Asset(db.Model, SerializerMixin):
     __tablename__ = "assets"
@@ -118,8 +152,9 @@ class Holdings(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     number_of_shares = db.Column(db.Float)
-    purchase_price = db.Column(db.Float)
+    purchase_price = db.Column(db.Float(precision=2))
     purchase_date = db.Column(db.Date)
+    client_name = db.Column(db.String)
 
     account_id = db.Column(db.Integer, db.ForeignKey('accounts.id'))
     asset_id = db.Column(db.Integer, db.ForeignKey('assets.id'))
@@ -137,8 +172,9 @@ class Holdings(db.Model, SerializerMixin):
             'purchase_date': self.purchase_date.isoformat(),
             'account_id': self.account_id,
             'asset_id': self.asset_id,
-            'client_id': self.client_id
+            'client_id': self.client_id,
+            'client_name': self.client_name
         }
 
     def __repr__(self):
-        return f'<Holdings {self.id}, {self.number_of_shares}, {self.purchase_price}, {self.purchase_date}>'
+        return f'<Holdings {self.id}, {self.number_of_shares}, {self.purchase_price}, {self.purchase_date}, {self.client_name}>'
